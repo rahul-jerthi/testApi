2c0a12f2e31f2401fec462225ca38216cdaac093
Rahul Jangid <112081189+rahul-jerthi@users.noreply.github.com>
2025-03-04 11:29:33 +0530

    Update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_API_KEY") 
